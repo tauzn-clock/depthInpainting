59eb9e32b9faeaad291d179ffebd902b96450656
Zhong Daoxin <tauzn-clock@github.com>
2025-07-05 05:38:41 +0000

    more clean up

--- src/tnnr.cpp
+++ src/tnnr.cpp
@@ -1,15 +1,13 @@
 #include "tnnr.h"
 
-inline float traceNorm(Mat &X)
-{
+inline float traceNorm(Mat &X){
   Mat w;
   SVD::compute(X,w);
   return sum(w)[0];
 }
 // mask 8-bit 1 channel 0 and 255
 // other matrices must be of CV_32FC1 type
-Mat APGL(Mat &A, Mat &B, Mat &X, Mat &M, Mat &mask, float eps, float lambda)
-{
+Mat APGL(Mat &A, Mat &B, Mat &X, Mat &M, Mat &mask, float eps, float lambda){
   Mat AB = A.t() * B;
   Mat BA = B.t() * A;
 
@@ -37,32 +35,30 @@ Mat APGL(Mat &A, Mat &B, Mat &X, Mat &M, Mat &mask, float eps, float lambda)
   float objval_last = 0.0;
   int k = 1;
 
-  for(k = 1; k < 201; k++)
-    {
-      Mat tmp;
-      multiply(Y-M, known, tmp);
-      Mat G = Y + t * (AB - lambda * tmp);
-      //svd for G
-      SVD::compute(G, sigma, u, v);
+  for(k = 1; k < 201; k++){
+    Mat tmp;
+    multiply(Y-M, known, tmp);
+    Mat G = Y + t * (AB - lambda * tmp);
+    //svd for G
+    SVD::compute(G, sigma, u, v);
 
-      sigma = max(sigma - t, 0.0);
-      sigma = Mat::diag(sigma);
-      X = u*sigma*v;
-      t = (1 + sqrt(1+4*tlast*tlast))/2;
-      Y = X + (tlast-1)/(t)*(X - lastX);
+    sigma = max(sigma - t, 0.0);
+    sigma = Mat::diag(sigma);
+    X = u*sigma*v;
+    t = (1 + sqrt(1+4*tlast*tlast))/2;
+    Y = X + (tlast-1)/(t)*(X - lastX);
 
-      X.copyTo(XX);
-      X.copyTo(lastX);
-      tlast = t;
+    X.copyTo(XX);
+    X.copyTo(lastX);
+    tlast = t;
 
-      multiply(X-M, known, tmp);
-      float tr = trace(X*BA)[0];
-      objval = traceNorm(X)  - tr + lambda / 2.0 * pow(norm(tmp,NORM_L2),2); 
-      if(k >= 2 && -(objval - objval_last) < eps) break;
+    multiply(X-M, known, tmp);
+    float tr = trace(X*BA)[0];
+    objval = traceNorm(X)  - tr + lambda / 2.0 * pow(norm(tmp,NORM_L2),2); 
+    if(k >= 2 && -(objval - objval_last) < eps) break;
 
-      objval_last = objval;
-    }
-
+    objval_last = objval;
+  }
   //  cout << "APGL total Iterations = " << k << endl;
   return XX;
 }
@@ -93,11 +89,10 @@ Mat TNNR(Mat &im0, Mat &mask, int lower_R, int upper_R, float lambda){
 
       X_rec = APGL(A, B, X, M, mask, eps, lambda);
 
-      if(out_iter >= 2 && norm(X_rec - X_rec_last) / norm(M,NORM_L2) < .01)
-        {
-          X_rec.copyTo(X);
-          break;
-        }
+      if(out_iter >= 2 && norm(X_rec - X_rec_last) / norm(M,NORM_L2) < .01){
+        X_rec.copyTo(X);
+        break;
+      }
 
       X_rec.copyTo(X);
       X_rec.copyTo(X_rec_last);	     
